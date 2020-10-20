25eaf83b95793e7ea9e2d804ce2abb6b5e64414c
ritz1231 <no.sven@web.de>
2020-10-20 16:19:40 +0200

    Better 06-2

--- Python Übungen/06-2-nGramme.py
+++ Python Übungen/06-2-nGramme.py
@@ -1,13 +1,15 @@
 # Aufgabe:  n-Gramme erstellen als Liste von Tuples
 from typing import List, Tuple, Generator
+import re
 # Generator
 def n_gram(n:int, text: str) -> Generator:
-    text_list = text.replace("!", "").replace(",","").replace(".","").split(" ")
+    text_list = re.sub("[!,.]", "", text).split(" ")
 
     x = 0
     while x+n <= len(text_list):
         yield tuple(text_list[x:x+n])
         x += 1 
 
-for a in n_gram(2, "1 2 3 4 5 6"):
-    print(a)
+
+text = "Hello there, General Kenobi. You're a bold one to assume that you can simple rickroll a being like me. Only a mere mortal like yours truly would fall for such a cheap trick."
+print( list(n_gram(10,text)) )
